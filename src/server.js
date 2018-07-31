ad85171c5c0a008bc257ff75914ac61d737368a9
Smorez <shinkirou@live.com>
2018-07-30 22:15:04 -0700

    fix

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use('/', index);
 app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../build/index.html'));
+    res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
 var port = process.env.PORT || 3001;
