252f36edb3c4c1129151841543932a1fbae92471
Smorez <shinkirou@live.com>
2018-08-06 14:59:10 -0700

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Gallery from "./Gallery.jsx";
 import NavBar from "./NavBar.jsx";
-import BootNav from "./BootNav.jsx";
 import Slides from "./Slides.jsx";
 
 class App extends Component {
